ab5ee0afbae0600eb6d820882eff3f7692cc4e72
Shai <shaisigawi@gmail.com>
2022-11-22 01:04:49 +0200

    ResetCounter

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,30 @@
 
 import './App.css';
 
-import {usestate} from "react";
+import {useState} from "react";
 
 function Counter() {
-    const [count, setCount] = usestate(1)
+    const [count, setCount] = useState(0)
     function incr(){
         setCount(
             function (oldCount){
                 return oldCount + 1
             }
         )
-        console.log(count)
+    }
+    function resetCounter(){
+        setCount(
+            function (oldCount){
+                return oldCount = 0
+            }
+        )
     }
   return (
     <div>
-        <h1>Counter</h1>
+        <h1>Counter App</h1>
         <p>Counter is at {count}</p>
         <button onClick={incr}>Click to add 1 to counter</button>
+        <b><button onClick={resetCounter}>Click to reset the counter</button></b>
     </div>
   );
 }
